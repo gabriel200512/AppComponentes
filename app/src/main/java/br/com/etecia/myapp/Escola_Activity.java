a9de86ba0dfde8fd4153abb93efdb145ab73a8fa
gabriel200512 <ffgabriel905@gmail.com>
2023-08-17 22:04:46 -0300

    janela de Menus

--- app/src/main/java/br/com/etecia/myapp/Escola_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Escola_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Menu_Activity extends AppCompatActivity {
+public class Escola_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menu_layout);
+        setContentView(R.layout.menudois_layout);
     }
 }
--- app/src/main/java/br/com/etecia/myapp/Mapa_Activity.java
+++ app/src/main/java/br/com/etecia/myapp/Mapa_Activity.java
@@ -4,11 +4,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class Menudois_Activity extends AppCompatActivity {
+public class Mapa_Activity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.menudois_layout);
+        setContentView(R.layout.menu_layout);
     }
 }
